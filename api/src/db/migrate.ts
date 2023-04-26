2b7de672742fa8acfa232d60051ac8bbb0eb4ec6
asasinmode <skin9470@gmail.com>
2023-04-26 21:07:38 +0200

    feat: update dependencies, remove migrations from git for now

--- api/src/db/migrate.ts
+++ api/src/db/migrate.ts
@@ -1,7 +1,11 @@
+import url from 'node:url';
 import { exit } from 'process';
 import { migrate } from 'drizzle-orm/mysql2/migrator';
 import { db } from './index';
 
-await migrate(db, { migrationsFolder: decodeURI(new URL('./migrations', import.meta.url).pathname.slice(1)) });
+await migrate(db, { migrationsFolder: url.fileURLToPath(new URL('./migrations', import.meta.url)) });
+
+// move to scripts directory
+// add drop/truncate? scripts
 
 exit(0);
--- api/src/db/migrations/0000_rainy_psynapse.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `users` (
-	`id` int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	`username` varchar(256) NOT NULL
-);
