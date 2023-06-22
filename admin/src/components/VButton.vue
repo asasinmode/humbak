fe4a228e7f695ac91c0c704ae170e33a35b76399
asasinmode <skin9470@gmail.com>
2023-06-22 18:24:36 +0200

    feat: use variant for hoverable

--- admin/src/components/VButton.vue
+++ admin/src/components/VButton.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-	<button class="w-fit border-2 border-zinc border-op-50 rounded-full bg-zinc bg-op-20 px-3 py-1 shadow dark:border-zinc-300 dark:border-op-80 dark:hoverable:border-op-100 hoverable:border-op-100 dark:bg-zinc-300 dark:bg-op-20 dark:hoverable:bg-opacity-30 hoverable:bg-opacity-30">
+	<button class="border-2 px-3 py-1 shadow neon-button-zinc">
 		<slot />
 	</button>
 </template>
--- admin/uno.config.ts
+++ admin/uno.config.ts
@@ -25,13 +25,24 @@ export default defineConfig({
 	shortcuts: [
 		{
 			'flex-center': 'flex justify-center items-center',
-			'text-link': 'op75 hoverable:(underline op100)',
 			'translate-center': 'translate-x--1/2 translate-y--1/2',
+			'text-link': 'op-75 hoverable:(underline op-100)',
 		},
-		[/^hoverable[:-](.+)$/, ([, c]) => `hover:${c} focus-visible:${c}`],
-		[/^neon-button[:-](.+)$/, ([, c]) => `border-2 border-${c} border-op-50 rounded-full bg-${c} bg-op-20 hover:bg-op-30 hover:border-op-100 focus-visible:bg-op-30 focus-visible:border-op-100 dark:border-op-80`],
+		[/^neon-button[:-](.+)$/, ([, c]) => `border-2 border-${c} border-op-50 rounded-full bg-${c} bg-op-20 hoverable:(bg-op-30 border-op-100) dark:border-op-80`],
 	],
 	rules: [
 		[/^grid-area-(.+)$/, ([, c]) => ({ 'grid-area': c })],
+	],
+	variants: [
+		(matcher) => {
+			if (matcher.slice(0, 9) !== 'hoverable') {
+				return matcher;
+			}
+
+			return {
+				matcher: matcher.slice(10),
+				selector: s => `${s}:hover, ${s}:focus-visible`,
+			};
+		},
 	],
 });
