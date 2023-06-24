be083d3022666f2259e1b70ad51b5f6e0b0b4dea
asasinmode <skin9470@gmail.com>
2023-06-24 09:27:29 +0200

    feat: init editor layout

--- admin/src/pages/PageHome.vue
+++ admin/src/pages/PageHome.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-	<section class="grid grid-cols-1 mx-auto max-w-7xl gap-x-4 gap-y-4 px-2 pt-4 md:grid-cols-3 md:px-4 md:py-8">
+	<section class="grid grid-cols-1 mx-auto max-w-7xl gap-x-4 gap-y-4 px-2 py-4 md:grid-cols-3 md:px-4 md:py-8">
 		<div class="col-span-full mt-[2px] w-[calc(100%-_3.5rem)] flex gap-4 md:mx-auto md:max-w-128">
 			<VInput id="pageSearch" class="w-0 flex-1" suffix-icon="i-fa6-solid-magnifying-glass" />
 			<VButton class="neon-blue">
@@ -15,13 +15,13 @@
 		<VInput id="pageMenuText" label="tekst w menu" />
 	</section>
 
-	<section class="h-[80vh] flex gap-4">
+	<section class="h-[60vh] flex resize-y gap-5 overflow-hidden">
 		<VEditor class="flex-1" />
-		<div class="flex-1 bg-pink-200/50" />
+		<main class="bg-checker flex-1" />
 	</section>
 
 	<section class="mt-4 flex justify-center gap-4 pb-4 md:mt-8">
-		<VButton class="neon-red">
+		<VButton class="-ml-[0.8rem] neon-red">
 			wyczyść
 		</VButton>
 		<VButton class="neon-green">
