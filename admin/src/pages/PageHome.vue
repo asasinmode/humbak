7153f36d182ebeb616becc32a87eb76736b1c948
asasinmode <skin9470@gmail.com>
2023-07-08 13:55:01 +0200

    feat: home page init page search

--- admin/src/pages/PageHome.vue
+++ admin/src/pages/PageHome.vue
@@ -45,14 +45,18 @@ function reset() {
 </script>
 
 <template>
-	<section class="grid grid-cols-[5fr_2fr] mx-auto max-w-6xl gap-x-4 gap-y-4 px-2 py-4 md:grid-cols-12 md:px-4 md:py-8">
-		<div class="col-span-full mt-[2px] w-[calc(100%-_3.5rem)] flex gap-4 md:mx-auto md:max-w-128">
-			<VInput id="pageSearch" class="flex-1" suffix-icon="i-solar-magnifer-linear" />
-			<VButton class="neon-blue">
-				szukaj
-			</VButton>
-		</div>
+	<section class="mb-4 w-[calc(100%-_3.5rem)] flex gap-4 px-2 pt-[18px] md:mx-auto md:max-w-128 md:px-0">
+		<VInput id="pageSearch" class="flex-1" suffix-icon="i-solar-magnifer-linear" />
+		<VButton class="neon-blue">
+			szukaj
+		</VButton>
+	</section>
 
+	<section class="mx-auto mb-4 max-w-208 bg-green px-2 md:mb-8 md:px-0">
+		here goes page select
+	</section>
+
+	<section class="grid grid-cols-[5fr_2fr] mx-auto mb-4 max-w-6xl gap-x-4 gap-y-4 px-2 md:grid-cols-12 md:mb-8 md:px-0">
 		<VInput
 			id="pageTitle"
 			v-model="title"
