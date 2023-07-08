6c2dec53af993fa2c231850782013aed512ee91e
asasinmode <skin9470@gmail.com>
2023-07-08 09:40:05 +0200

    feat: use before for toast icon bg

--- admin/src/components/TheToasts.vue
+++ admin/src/components/TheToasts.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import type { ToastVariant } from '~/composables/useToast';
 
-const { _toasts, clearToast } = useToast();
+const { toasts, clearToast } = useToast();
 
 type ClassRecord = Record<ToastVariant, string>;
 
@@ -9,37 +9,32 @@ const icon: ClassRecord = {
 	success: 'i-solar-check-circle-linear text-green',
 	error: 'i-solar-close-circle-linear text-red',
 };
-const iconBackground: ClassRecord = {
-	success: 'bg-green',
-	error: 'bg-red',
-};
 const containerProgress: ClassRecord = {
-	success: 'after:bg-green',
-	error: 'after:bg-red',
+	success: 'after:bg-green before:bg-green',
+	error: 'after:bg-red before:bg-red',
 };
 </script>
 
 <template>
 	<section
 		class="fixed right-2 top-2 z-100 max-h-screen max-w-[calc(100%_-_4rem)] w-64 flex flex-col gap-3 overflow-y-hidden"
-		:class="{ 'pb-2': _toasts.length }"
+		:class="{ 'pb-2': toasts.length }"
 		aria-hidden
 	>
 		<TransitionGroup>
 			<div
-				v-for="{ id, variant, text } in _toasts"
+				v-for="{ id, variant, text } in toasts"
 				:key="id"
-				class="toast-progress relative min-h-12 w-full flex shrink-0 items-center gap-3 overflow-hidden rounded-md bg-neutral-200 p-2 pb-3 hyphens-auto shadow-md after:absolute after:bottom-0 after:left-0 after:h-1 after:w-0 dark:bg-neutral-800 after:content-empty"
+				class="toast-progress relative min-h-12 w-full flex shrink-0 items-center overflow-hidden rounded-md bg-neutral-200 p-2 pb-3 pl-11 hyphens-auto shadow-md after:absolute before:absolute after:bottom-0 after:left-0 before:left-2 before:top-1/2 after:h-1 after:w-0 before:h-8 before:w-8 before:rounded-1/2 dark:bg-neutral-800 before:bg-op-20 after:content-empty before:content-empty before:-translate-y-1/2"
 				:class="containerProgress[variant]"
 			>
-				<div class="h-8 w-8 flex-center shrink-0 rounded-1/2 bg-op-20" :class="iconBackground[variant]">
-					<div class="h-6 w-6" :class="icon[variant]" />
-				</div>
+				<div class="absolute left-3 top-1/2 h-6 w-6 -translate-y-1/2" :class="icon[variant]" />
 				{{ text }}
 				<button
-					class="absolute right-1 top-1 text-2 text-neutral w-5 h-5 i-mdi-close hover:text-neutral-500 dark:hover:text-neutral-300"
+					class="i-mdi-close absolute right-1 top-1 h-5 w-5 text-2 text-neutral hover:text-neutral-500 dark:hover:text-neutral-300"
 					tabindex="-1"
-					@click="clearToast(id)" />
+					@click="clearToast(id)"
+				/>
 			</div>
 		</TransitionGroup>
 	</section>
--- admin/src/composables/useToast.ts
+++ admin/src/composables/useToast.ts
@@ -5,29 +5,29 @@ type Toast = {
 	text: string;
 };
 
-const _toasts = ref<Toast[]>([]);
+const toasts = ref<Toast[]>([]);
 const timeouts: Record<string, NodeJS.Timeout> = {};
 
 export const useToast = () => ({
-	_toasts,
+	toasts,
 	toast(text: string, variant: ToastVariant = 'success') {
 		const id = Math.random().toString(36).substring(2, 9);
 
-		_toasts.value.push({
+		toasts.value.push({
 			id,
 			text,
 			variant,
 		});
-		timeouts[id] = setTimeout(() => _toasts.value.shift(), 2500);
+		timeouts[id] = setTimeout(() => toasts.value.shift(), 2500);
 	},
 	clearToast(id: string) {
-		const index = _toasts.value.findIndex(toast => toast.id === id);
+		const index = toasts.value.findIndex(toast => toast.id === id);
 
 		if (!timeouts[id] || index === -1) {
 			return;
 		}
 
-		_toasts.value.splice(index, 1);
+		toasts.value.splice(index, 1);
 		clearTimeout(timeouts[id]);
 	},
 });
