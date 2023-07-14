9890a7b1716e6ae3d76d5d33fa155598b95a64cd
asasinmode <skin9470@gmail.com>
2023-07-14 14:47:48 +0200

    feat: more table styles

--- admin/src/components/V/VTable.vue
+++ admin/src/components/V/VTable.vue
@@ -3,38 +3,51 @@ const props = defineProps<{
 	rows: T[];
 	total: number;
 	rowKey: keyof T;
+	keyLabels: Record<keyof T, string>;
+	caption: string;
 }>();
 
 function getRowKey(row: T) {
 	return `${row[props.rowKey]}`;
 }
+function getKeyLabel(key: keyof T) {
+	return `${props.keyLabels[key]}`;
+}
 </script>
 
 <template>
-	<table class="v-table w-full">
-		<caption class="text-start">
-			strony {{ total }}
-		</caption>
-		<tr>
-			<th class="p-4 text-start">
-				id
-			</th>
-			<th class="p-4 text-start">
-				tytuł
-			</th>
-			<th class="p-4 text-start">
-				tekst w m
-			</th>
-			<th class="p-4 text-start">
-				język
-			</th>
-		</tr>
-		<tr v-for="row in rows" :key="getRowKey(row)">
-			<td v-for="(value, key) in row" :key="key" class="p-4">
-				{{ key }}: {{ value }}
-			</td>
-		</tr>
-	</table>
+	<div class="overflow-auto" role="region" tabindex="0">
+		<table class="v-table w-full" role="table">
+			<caption class="text-start">
+				{{ caption }} {{ total }}
+			</caption>
+			<tr class="" role="row">
+				<th
+					v-for="(label, key) in keyLabels"
+					:key="key"
+					class="text-start"
+					role="columnheader"
+				>
+					{{ label }}
+				</th>
+			</tr>
+			<tr
+				v-for="row in rows"
+				:key="getRowKey(row)"
+				class=""
+				role="row"
+			>
+				<td
+					v-for="(value, key) in row"
+					:key="key"
+					:data-cell="getKeyLabel(key)"
+					role="cell"
+				>
+					{{ value }}
+				</td>
+			</tr>
+		</table>
+	</div>
 </template>
 
 <style>
@@ -43,8 +56,30 @@ function getRowKey(row: T) {
 		display: none;
 	}
 
-	.v-table td {
+	.v-table tr {
 		display: block;
+		padding-inline: 0.5rem;
+		padding-block: clamp(0.50rem, calc(0.23rem + 1.34vw), 0.88rem);
+	}
+
+	.v-table td {
+		display: grid;
+		grid-template-columns: 6.25rem auto;
+		gap: 1rem;
+		font-weight: 700;
+	}
+
+	.v-table td::before {
+		content: attr(data-cell) ":";
+		font-weight: 400;
+	}
+
+	.v-table tr:nth-of-type(2n) {
+		background-color: hsl(0 0% 0% / 0.1);
+	}
+
+	.dark .v-table tr:nth-of-type(2n) {
+		background-color: hsl(0 0% 100% / 0.1);
 	}
 }
 </style>
