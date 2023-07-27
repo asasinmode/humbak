76d5b47365b32474da018316a37b05371ee2adc9
asasinmode <skin9470@gmail.com>
2023-07-27 20:49:04 +0200

    refactor: api return types

--- admin/src/composables/useApi.ts
+++ admin/src/composables/useApi.ts
@@ -12,8 +12,5 @@ const client = createTRPCProxyClient<AppRouter>({
 
 export const useApi = () => client;
 
-const listPagesQuery = client.pages.list.query;
-export type ListedPage = Awaited<ReturnType<typeof listPagesQuery>>[number];
-
-const uniqueLanguagesQuery = client.pages.uniqueLanguages.query;
-export type UniqueLanguage = Awaited<ReturnType<typeof uniqueLanguagesQuery>>[number];
+export type ListedPage = Awaited<ReturnType<typeof client['pages']['list']['query']>>[number];
+export type UniqueLanguage = Awaited<ReturnType<typeof client['pages']['uniqueLanguages']['query']>>[number];
