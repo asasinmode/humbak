f740a0090c67c7a627653d2e2b173637b7e043c8
asasinmode <skin9470@gmail.com>
2023-07-31 18:55:13 +0200

    feat: editor meta add schema

--- admin/src/components/V/VEditor.vue
+++ admin/src/components/V/VEditor.vue
@@ -18,13 +18,13 @@ const emit = defineEmits<{
 	'update:model-value': [string];
 }>();
 
+const { isDark } = useTheme();
+
 const isLoading = ref(false);
 const editorRef = ref<HTMLDivElement>();
 const editor = shallowRef<IMonacoStandalone>();
 const editorModels = shallowRef<IMonacoTextModel[]>([]);
 let setTheme: (theme: string) => void;
-
-const { isDark } = useTheme();
 
 // add loading & load only once
 onMounted(async () => {
@@ -38,9 +38,67 @@ onMounted(async () => {
 	setTheme = monaco.editor.setTheme;
 	setTheme(isDark.value ? 'vs-dark' : 'vs');
 
+	const metaUri = monaco.Uri.parse('meta.json');
+
 	for (const { value, language } of props.models) {
-		editorModels.value.push(monaco.editor.createModel(value, language));
+		editorModels.value.push(
+			monaco.editor.createModel(value, language, language === 'json' ? metaUri : undefined)
+		);
 	}
+
+	monaco.languages.json.jsonDefaults.setDiagnosticsOptions({
+		validate: true,
+		schemas: [{
+			uri: 'metaSchema.json',
+			fileMatch: [metaUri.toString()],
+			schema: {
+				type: 'array',
+				items: {
+					type: 'object',
+					properties: {
+						name: {
+							type: 'string',
+							description: [
+								'The name and content attributes can be used together to provide document metadata in terms of name-value pairs,',
+								'with the name attribute giving the metadata name, and the content attribute giving the value.',
+								'',
+								'@see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta#attr-name',
+							].join('\n'),
+							anyOf: [
+								{
+									enum: [
+										'description',
+										'author',
+										'creator',
+										'publisher',
+										'generator',
+										'referrer',
+										'robots',
+										'google',
+										'googlebot',
+										'rating',
+										'format-detection',
+										'x-ua-compatible',
+										'refresh',
+										'keywords',
+									],
+								},
+								{ pattern: '.*' },
+							],
+						},
+						content: {
+							description: [
+								'This attribute contains the value for the http-equiv or name attribute, depending on which is used.',
+								'@see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/meta#attr-content',
+							].join('\n'),
+							type: 'string',
+						},
+					},
+					additionalProperties: true,
+				},
+			},
+		}],
+	});
 
 	const editorInstance = monaco.editor.create(editorRef.value, {
 		model: editorModels.value[props.currentModel],
