43ee0448d1319bec99dd1821be8c14e1b85723d2
asasinmode <skin9470@gmail.com>
2023-08-08 11:46:55 +0200

    feat: fastify static serve stylesheets

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,7 @@
+import { fileURLToPath } from 'node:url';
 import fastify from 'fastify';
 import cors from '@fastify/cors';
+import fastifyStatic from '@fastify/static'
 import { fastifyTRPCPlugin } from '@trpc/server/adapters/fastify';
 import { createContext } from '~/router/context';
 import { appRouter } from '~/router';
@@ -14,6 +16,11 @@ await server.register(cors, {
 server.get('/', (_, res) => {
 	return res.send('hi');
 });
+
+await server.register(fastifyStatic, {
+	root: fileURLToPath(new URL('../public', import.meta.url)),
+	prefix: '/public/'
+})
 
 await server.register(fastifyTRPCPlugin, {
 	prefix: '/',
