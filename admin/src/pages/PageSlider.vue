1dd580de1a14573a2ace5b3fe68910cced568915
asasinmode <skin9470@gmail.com>
2023-08-22 19:51:02 +0200

    feat: slider resize button position

--- admin/src/pages/PageSlider.vue
+++ admin/src/pages/PageSlider.vue
@@ -6,8 +6,12 @@ const { initResizeDrag } = useResizeHandler(container);
 </script>
 
 <template>
-	<main class="mx-auto max-w-256 gap-x-3 gap-y-5 px-4 pb-4 pt-[3.625rem] lg:px-0 lg:pt-[1.125rem]">
-		<section ref="container" class="relative min-h-[max(60vh,_16rem)] w-full flex">
+	<main class="gap-x-3 gap-y-5 px-4 pb-4 pt-[3.625rem] lg:px-0 md:pt-[1.125rem]">
+		<section
+			id="sliderEditorContainer"
+			ref="container"
+			class="relative mx-auto max-w-256 min-h-[min(60vh,_16rem)] w-full flex"
+		>
 			<VEditor
 				ref="editor"
 				class="flex-1 shadow"
@@ -18,7 +22,8 @@ const { initResizeDrag } = useResizeHandler(container);
 				@update:model-value="$event => value = $event"
 			/>
 			<VButton
-				class="top-1/2 hidden h-8 w-8 translate-center cursor-move p-0 !absolute -right-4 md:flex neon-neutral"
+				id="sliderEditorResize"
+				class="right-0 top-1/2 hidden h-8 w-8 cursor-move p-0 !absolute md:flex -translate-y-1/2 neon-neutral"
 				title="zmień rozmiar"
 				@mousedown="initResizeDrag"
 			>
@@ -28,3 +33,11 @@ const { initResizeDrag } = useResizeHandler(container);
 		</section>
 	</main>
 </template>
+
+<style>
+@media (min-width: 768px) {
+	#sliderEditorContainer {
+		padding-inline: clamp(2.00rem, calc(-1.00rem + 6.25vw), 3.00rem);
+	}
+}
+</style>
