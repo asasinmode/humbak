eb5f02e6e6a934b3039ec6d27e7b72c3ad67506d
asasinmode <skin9470@gmail.com>
2023-09-17 10:07:39 +0200

    feat: change slider layout

--- admin/src/pages/PageCss.vue
+++ admin/src/pages/PageCss.vue
@@ -32,13 +32,13 @@ function updateModelValue(newValue: string) {
 </script>
 
 <template>
-	<main class="grid grid-cols-[1fr_min-content] grid-rows-[min-content_1fr] mx-auto max-w-256 min-h-inherit w-full flex-1 gap-x-3 gap-y-5 px-4 pb-4 pt-[1.125rem] lg:px-0">
+	<main class="grid grid-cols-[1fr_min-content] grid-rows-[min-content_1fr] mx-auto max-w-256 min-h-inherit w-full gap-x-3 gap-y-5 px-4 pb-4 pt-[1.125rem] lg:px-0">
 		<VButton class="h-9 w-9 justify-self-end p-0 neon-purple" title="formatuj" @click="editor?.formatCurrentModel">
 			<span class="visually-hidden">formatuj</span>
 			<div class="i-solar-magic-stick-3-bold absolute left-1/2 top-1/2 h-5 w-5 translate-center" />
 		</VButton>
 		<VButton
-			class="ml-auto mr-12 md:mr-0 neon-green"
+			class="mr-12 md:mr-0 neon-green"
 			:is-loading="isSaving"
 			@click="saveChanges"
 		>
--- admin/src/pages/PageSlider.vue
+++ admin/src/pages/PageSlider.vue
@@ -1,43 +1,22 @@
 <script setup lang="ts">
 const value = ref('');
-const container = ref<HTMLDivElement>();
-
-const { initResizeDrag } = useResizeHandler(container);
 </script>
 
 <template>
-	<main class="gap-x-3 gap-y-5 px-4 pb-4 pt-[3.625rem] lg:px-0 md:pt-[1.125rem]">
-		<section
-			id="sliderEditorContainer"
-			ref="container"
-			class="relative mx-auto max-w-256 min-h-[min(60vh,_16rem)] w-full flex"
+	<main class="grid grid-cols-1 mx-auto max-w-256 w-full gap-y-5 px-4 pb-4 pt-[1.125rem] lg:px-0">
+		<VButton
+			class="mr-12 justify-self-end md:mr-0 neon-green"
 		>
-			<VEditor
-				ref="editor"
-				class="flex-1 shadow"
-				:models="[
-					{ language: 'html', value },
-				]"
-				:current-model="0"
-				@update:model-value="$event => value = $event"
-			/>
-			<VButton
-				id="sliderEditorResize"
-				class="right-0 top-1/2 hidden h-8 w-8 cursor-move p-0 !absolute md:flex -translate-y-1/2 neon-neutral"
-				title="zmień rozmiar"
-				@mousedown="initResizeDrag"
-			>
-				<span class="visually-hidden">zmień rozmiar</span>
-				<div class="i-fa6-solid-arrows-up-down absolute left-1/2 top-1/2 h-4 w-4 translate-center" />
-			</VButton>
-		</section>
+			zapisz
+		</VButton>
+		<VEditor
+			ref="editor"
+			class="h-72 shadow"
+			:models="[
+				{ language: 'html', value },
+			]"
+			:current-model="0"
+			@update:model-value="$event => value = $event"
+		/>
 	</main>
 </template>
-
-<style>
-@media (min-width: 768px) {
-	#sliderEditorContainer {
-		padding-inline: clamp(2.00rem, calc(-1.00rem + 6.25vw), 3.00rem);
-	}
-}
-</style>
