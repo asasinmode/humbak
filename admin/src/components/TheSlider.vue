447b7b67a4cb760d6f8c954662889677543af5e5
asasinmode <skin9470@gmail.com>
2023-09-29 20:01:59 +0200

    feat: init theslider functionality

--- admin/src/components/TheSlider.vue
+++ admin/src/components/TheSlider.vue
@@ -1,9 +1,45 @@
 <script setup lang="ts">
-defineProps<{
+import type { IPublicListedSlide } from '~/composables/useApi';
+
+const props = defineProps<{
 	language?: string;
+	aspectRatio?: string;
 }>();
+
+const isLoading = ref(true);
+const slides = ref<IPublicListedSlide[]>([]);
+
+onMounted(() => {
+	console.log('if', props.language, 'and', props.aspectRatio, 'then fetch, otherwise stay loading');
+});
+
+async function handleSlide(id: number, content: string) {
+	const slideIndex = slides.value.findIndex(slide => slide.id === id);
+
+	if (slideIndex === -1) {
+		console.log('new slide, pushing', { id, content });
+		return;
+	}
+
+	console.log('changing existing slide', { id, content });
+}
+
+async function loadSlies() {
+	console.log('loading slides for', props.language, 'with', props.aspectRatio);
+}
+
+defineExpose({
+	handleSlide,
+});
 </script>
 
 <template>
-	<div>henlo a preview for {{ language }}</div>
+	<article
+		class="relative w-full"
+		:style="{ paddingTop: `calc(${aspectRatio} * 100%)` }"
+		aria-hidden
+		tabindex="-1"
+	>
+		<div ref="container" class="absolute inset-0" />
+	</article>
 </template>
