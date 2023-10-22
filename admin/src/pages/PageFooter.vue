d43b8508d1a8e53b5cf1e6ff31872ba8f6d8f360
asasinmode <skin9470@gmail.com>
2023-10-22 11:20:22 +0200

    feat: social links shadow

--- admin/src/pages/PageFooter.vue
+++ admin/src/pages/PageFooter.vue
@@ -286,7 +286,7 @@ function addSocial() {
 					:title="`link ${social.type}`"
 					:href="social.value"
 					target="_blank"
-					class="transition-transform hoverable:scale-120"
+					class="drop-shadow transition-transform hoverable:scale-120"
 				>
 					<span class="visually-hidden">link {{ social.type }}</span>
 					<div class="h-8 w-8" :class="socialToIcon[social.type]" />
