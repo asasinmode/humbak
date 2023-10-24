61ff2237c1fe38d07b5765ab388c695c3097c629
asasinmode <skin9470@gmail.com>
2023-10-24 19:45:38 +0200

    feat: init files

--- admin/src/components/V/VSwitch.vue
+++ admin/src/components/V/VSwitch.vue
@@ -35,7 +35,7 @@ const value = defineModel<boolean>();
 			v-if="label"
 			:id="`${id}Label`"
 			:for="id"
-			class="v-switch-label relative ml-3 w-fit after:(absolute bottom-[calc(-0.125rem_-_2px)] h-[calc(2.25rem_-_4px)] w-[calc(2.25rem_-_4px)] translate-y-full rounded-1/2 bg-neutral-8 transition-transform content-empty -left-[calc(0.75rem_-_2px)] dark:bg-neutral-2) before:(absolute bottom-[-0.125rem] h-9 w-[calc(4.5rem_-_4px)] translate-y-full rounded-full content-empty -left-3 neon-neutral)"
+			class="v-switch-label relative ml-3 w-fit after:(absolute bottom-[calc(-0.125rem_-_2px)] h-[calc(2.25rem_-_4px)] w-[calc(2.25rem_-_4px)] translate-y-full rounded-1/2 bg-neutral-8 transition-transform content-empty -left-[calc(0.75rem_-_2px)] dark:bg-neutral-2) before:(absolute bottom-[-0.125rem] h-9 w-[calc(4.5rem_-_4px)] translate-y-full neon-neutral rounded-full content-empty -left-3)"
 			:class="labelVisuallyHidden ? 'visually-hidden' : ''"
 		>
 			{{ label }}
--- admin/src/pages/PageFiles.vue
+++ admin/src/pages/PageFiles.vue
@@ -2,7 +2,14 @@
 </script>
 
 <template>
-	<main id="content" class="flex-center">
-		pliki
+	<main id="content" class="flex flex-col gap-x-3 gap-y-5 pb-4 pt-[1.125rem]">
+		<div class="mx-auto max-w-360 w-full flex justify-end">
+			<VButton
+				ref="saveButton"
+				class="footer-controls-padding-right mr-12 h-fit md:mr-0 neon-green"
+			>
+				zapisz
+			</VButton>
+		</div>
 	</main>
 </template>
