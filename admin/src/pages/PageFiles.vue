e629d9479fe031be662f180bb2654ea83f06b8ed
asasinmode <skin9470@gmail.com>
2023-10-27 18:34:42 +0200

    feat: init toggle buttons

--- admin/src/pages/PageFiles.vue
+++ admin/src/pages/PageFiles.vue
@@ -5,6 +5,16 @@ const isTiles = ref(true);
 <template>
 	<main id="content" class="flex flex-col gap-x-3 gap-y-5 pb-4 pt-[1.125rem]">
 		<div class="mx-auto max-w-360 w-full flex justify-end gap-x-3">
+			<VButton class="h-9 w-9 neon-blue" title="widok plików: kafelki" @click="isTiles = true">
+				<span class="visually-hidden">widok plików: kafelki</span>
+				<div class="i-fluent-grid-20-regular absolute left-1/2 top-1/2 h-[1.125rem] w-[1.125rem] translate-center" />
+			</VButton>
+
+			<VButton class="h-9 w-9 -ml-1 neon-blue" title="widok plików: lista" @click="isTiles = false">
+				<span class="visually-hidden">widok plików: lista</span>
+				<div class="i-fluent-text-bullet-list-20-filled absolute left-1/2 top-1/2 h-[1.375rem] w-[1.375rem] translate-center" />
+			</VButton>
+
 			<VButton
 				ref="saveButton"
 				class="footer-controls-padding-right mr-12 h-fit md:mr-0 neon-green"
@@ -12,7 +22,7 @@ const isTiles = ref(true);
 				zapisz
 			</VButton>
 		</div>
-		<div class="grid auto-rows-[9rem] grid-cols-[repeat(auto-fit,minmax(9rem,1fr))] mx-auto max-w-360 w-full gap-x-5 gap-y-4 px-2 lg:px-8 md:px-[clamp(2rem,_-12.25rem_+_29.6875vw,_6.75rem)]">
+		<div class="grid auto-rows-[9rem] grid-cols-[repeat(auto-fit,minmax(9rem,1fr))] mx-auto max-w-360 w-full gap-x-5 gap-y-4">
 			<div class="flex items-center border border-neutral rounded-md">
 				dodaj folder/wgraj pliki
 			</div>
