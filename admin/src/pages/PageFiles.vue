afa54cc511b1a2a0cceb8d57de15cbc00b7039a5
asasinmode <skin9470@gmail.com>
2023-11-02 09:16:54 +0100

    feat: init file styles

--- admin/src/pages/PageFiles.vue
+++ admin/src/pages/PageFiles.vue
@@ -1,10 +1,28 @@
 <script setup lang="ts">
 const isTiles = ref(true);
+
+const classContainer = computed(() => isTiles.value
+	? 'grid auto-rows-[9rem] grid-cols-[repeat(auto-fit,minmax(9rem,1fr))]'
+	: ''
+);
+
+const classChild = computed(() => isTiles.value
+	? ''
+	: ''
+);
+
+const files = [
+	{ id: 0, name: 'file1', title: 'file1', alt: 'file1', src: 'https://picsum.photos/500', mimetype: 'image/jpeg' },
+	{ id: 1, name: 'file2', title: 'file2', alt: 'file2', src: 'https://picsum.photos/501', mimetype: 'image/jpeg' },
+	{ id: 2, name: 'file3', title: 'file3', alt: 'file3', src: 'https://picsum.photos/502', mimetype: 'image/jpeg' },
+	{ id: 3, name: 'file4', title: 'file4', alt: 'file4', src: 'https://picsum.photos/503', mimetype: 'image/jpeg' },
+	{ id: 4, name: 'file5', title: 'file5', alt: 'file5', src: 'https://picsum.photos/504', mimetype: 'image/jpeg' },
+];
 </script>
 
 <template>
 	<main id="content" class="flex flex-col gap-x-3 gap-y-5 pb-4 pt-[1.125rem]">
-		<div class="px-container mx-auto max-w-360 w-full flex justify-end gap-x-3 md:px-0">
+		<div class="mx-auto max-w-360 w-full flex justify-end gap-x-3 px-container md:px-0">
 			<VButton class="h-9 w-9 neon-blue" title="widok plików: kafelki" @click="isTiles = true">
 				<span class="visually-hidden">widok plików: kafelki</span>
 				<div class="i-fluent-grid-20-regular absolute left-1/2 top-1/2 h-[1.125rem] w-[1.125rem] translate-center" />
@@ -17,19 +35,16 @@ const isTiles = ref(true);
 
 			<VButton
 				ref="saveButton"
-				class="md:mr-container mr-12 h-fit neon-green"
+				class="mr-12 h-fit md:mr-container neon-green"
 			>
 				zapisz
 			</VButton>
 		</div>
-		<div class="px-container grid auto-rows-[9rem] grid-cols-[repeat(auto-fit,minmax(9rem,1fr))] mx-auto max-w-360 w-full gap-x-5 gap-y-4">
-			<div class="flex items-center border border-2 border-neutral rounded-lg">
-				dodaj folder/wgraj pliki
+		<div class="mx-auto max-w-360 w-full gap-x-5 gap-y-4 px-container" :class="classContainer">
+			<div v-for="file in files" :key="file.id" :class="classChild">
+				<img :src="file.src" :title="file.title" :alt="file.alt">
 			</div>
-			<div class="flex items-center border border-2 border-neutral rounded-lg">
-				dodaj folder/wgraj pliki
-			</div>
-			<div class="flex items-center border border-2 border-neutral rounded-lg">
+			<div :class="classChild">
 				dodaj folder/wgraj pliki
 			</div>
 		</div>
