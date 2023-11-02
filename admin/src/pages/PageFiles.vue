c33919c68d8a559ecc39d8ea515064c61df51f15
asasinmode <skin9470@gmail.com>
2023-11-02 18:02:14 +0100

    feat: small screen list style

--- admin/src/pages/PageFiles.vue
+++ admin/src/pages/PageFiles.vue
@@ -5,21 +5,19 @@ type IFile = { id: number; parentId: null | number; title: string; alt: string; 
 const isTiles = ref(true);
 
 const classes = computed(() => {
-	if (isTiles.value) {
-		return {
-			container: 'grid grid-rows-[clamp(7rem,_6.1579rem_+_4.2105vw,_9rem)_auto_auto_auto_auto] grid-cols-[repeat(auto-fill,minmax(17rem,1fr))] gap-y-4',
-			child: 'grid grid-cols-2 gap-x-3 grid-rows-subgrid pb-4 gap-y-3 row-span-5 items-center',
-			image: 'w-full h-[clamp(7rem,_6.1579rem_+_4.2105vw,_9rem)] mb-1 col-span-full',
-			input: 'col-span-full mx-3',
-		};
+	let container = 'grid grid-rows-[clamp(7rem,_6.1579rem_+_4.2105vw,_9rem)_auto_auto_auto_auto] grid-cols-[repeat(auto-fill,minmax(17rem,1fr))] gap-y-4';
+	let child = 'grid grid-cols-2 gap-x-3 grid-rows-subgrid pb-4 gap-y-3 row-span-5 items-center';
+	let image = 'w-full h-[clamp(7rem,_6.1579rem_+_4.2105vw,_9rem)] mb-1 col-span-full';
+	let input = 'col-span-full mx-3';
+
+	if (!isTiles.value) {
+		container += ' flex flex-col';
+		child += ' sm:flex sm:gap-3 sm:pr-4 sm:pb-0';
+		image += ' sm:h-20 sm:w-20 sm:mb-0';
+		input += ' sm:mx-0';
 	}
 
-	return {
-		container: 'flex flex-col gap-y-4',
-		child: 'w-full flex flex-row gap-3 pr-4',
-		image: 'h-20 w-20',
-		input: 'mt-2',
-	};
+	return { container, child, image, input };
 });
 
 const isLoading = ref(false);
@@ -69,12 +67,12 @@ function randomImageSrc(id: number) {
 <template>
 	<main id="content" class="flex flex-col gap-x-3 gap-y-5 pb-4 pt-[1.125rem]">
 		<div class="mx-auto max-w-360 w-full flex justify-end gap-x-3 px-container md:px-0">
-			<VButton class="h-9 w-9 shrink-0 neon-blue" title="widok plików: kafelki" @click="isTiles = true">
+			<VButton class="h-9 w-9 shrink-0 sm:flex neon-blue" title="widok plików: kafelki" @click="isTiles = true">
 				<span class="visually-hidden">widok plików: kafelki</span>
 				<div class="i-fluent-grid-20-regular absolute left-1/2 top-1/2 h-[1.125rem] w-[1.125rem] translate-center" />
 			</VButton>
 
-			<VButton class="h-9 w-9 shrink-0 -ml-1 neon-blue" title="widok plików: lista" @click="isTiles = false">
+			<VButton class="h-9 w-9 shrink-0 -ml-1 sm:flex neon-blue" title="widok plików: lista" @click="isTiles = false">
 				<span class="visually-hidden">widok plików: lista</span>
 				<div class="i-fluent-text-bullet-list-20-filled absolute left-1/2 top-1/2 h-[1.375rem] w-[1.375rem] translate-center" />
 			</VButton>
@@ -87,18 +85,18 @@ function randomImageSrc(id: number) {
 			</VButton>
 		</div>
 		<div class="mx-auto max-w-360 w-full gap-x-5 px-container" :class="classes.container">
-			<div class="flex border-2 border-neutral rounded-lg shadow" :class="isTiles ? ' row-span-5 flex-col' : 'h-20'">
-				<div class="flex basis-1/2 items-center justify-center gap-3 border-neutral px-3 py-4" :class="isTiles ? 'flex-col border-b' : 'flex-row border-r'">
+			<div class="flex flex-col border-2 border-neutral rounded-lg shadow" :class="isTiles ? 'row-span-5' : 'sm:flex-row'">
+				<div class="flex basis-1/2 flex-col items-center justify-center gap-3 border-b border-neutral px-3 py-4" :class="isTiles ? '' : 'sm:flex-row sm:border-b-0 sm:border-r'">
 					<VInput
 						id="newDirName"
 						v-model="newDirName"
 						label="nazwa folderu"
 					/>
-					<VButton class="h-fit shrink-0 neon-green" :class="isTiles ? '' : 'mt-[1.625rem]'">
+					<VButton class="h-fit shrink-0 neon-green" :class="isTiles ? '' : 'sm:mt-[1.625rem]'">
 						nowy folder
 					</VButton>
 				</div>
-				<div class="flex basis-1/2 items-center justify-center border-neutral px-3 py-4" :class="isTiles ? 'border-t' : 'border-l'">
+				<div class="flex basis-1/2 items-center justify-center border-t border-neutral px-3 py-4" :class="isTiles ? '' : 'sm:border-t-0 sm:border-l'">
 					<VButton class="neon-blue">
 						wgraj pliki
 					</VButton>
@@ -130,10 +128,10 @@ function randomImageSrc(id: number) {
 					label="ścieżka"
 					:class="classes.input"
 				/>
-				<VButton class="h-fit w-fit neon-red" :class="isTiles ? 'mt-3 mr-2 justify-self-end' : 'ml-3 mt-auto mb-[0.625rem]'">
+				<VButton class="mr-2 mt-3 h-fit w-fit justify-self-end neon-red" :class="isTiles ? '' : 'sm:ml-3 sm:mt-auto sm:mr-0 sm:mb-[0.625rem]'">
 					usuń
 				</VButton>
-				<VButton class="h-fit w-fit neon-blue" :class="isTiles ? 'mt-3 -ml-2' : 'mt-auto mb-[0.625rem]'">
+				<VButton class="mt-3 h-fit w-fit -ml-2 neon-blue" :class="isTiles ? '' : 'sm:mt-auto sm:ml-0 sm:mb-[0.625rem]'">
 					przenieś
 				</VButton>
 			</article>
