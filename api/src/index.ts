6e23d9a89661e50bdcf54c7a6ee87fe06ec70d86
asasinmode <skin9470@gmail.com>
2023-12-02 14:38:31 +0100

    refactor: specific app names

--- api/src/index.ts
+++ api/src/index.ts
@@ -3,12 +3,12 @@ import { cors } from 'hono/cors';
 import { serve } from '@hono/node-server';
 import { serveStatic } from '@hono/node-server/serve-static';
 import { env } from './env';
-import { app as footerContentsRoute } from './routes/footerContents';
-import { app as globalCssRoute } from './routes/globalCss';
-import { app as languagesRoute } from './routes/languages';
-import { app as menuLinksRoute } from './routes/menuLinks';
-import { app as pagesRoute } from './routes/pages';
-import { app as slidesRoute } from './routes/slides';
+import { app as footerContentsApp } from './routes/footerContents';
+import { app as globalCssApp } from './routes/globalCss';
+import { app as languagesApp } from './routes/languages';
+import { app as menuLinksApp } from './routes/menuLinks';
+import { app as pagesApp } from './routes/pages';
+import { app as slidesApp } from './routes/slides';
 
 const app = new Hono();
 
@@ -25,12 +25,12 @@ app.use('/public/*', serveStatic({
 }));
 
 const typedApp = app
-	.route('/footerContents', footerContentsRoute)
-	.route('/globalCss', globalCssRoute)
-	.route('/languages', languagesRoute)
-	.route('/menuLinks', menuLinksRoute)
-	.route('/pages', pagesRoute)
-	.route('/slides', slidesRoute);
+	.route('/footerContents', footerContentsApp)
+	.route('/globalCss', globalCssApp)
+	.route('/languages', languagesApp)
+	.route('/menuLinks', menuLinksApp)
+	.route('/pages', pagesApp)
+	.route('/slides', slidesApp);
 
 serve({ port: env.PORT, fetch: app.fetch }, (info) => {
 	console.log(`server listening on\x1B[36m http://localhost:${info.port}/ \x1B[0m`);
