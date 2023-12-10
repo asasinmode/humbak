9ed5c46374640a8765a25d974e0b4e8a46640214
asasinmode <skin9470@gmail.com>
2023-12-10 08:29:00 +0100

    fix: truncate script timing out

--- api/src/db/scripts/truncate.ts
+++ api/src/db/scripts/truncate.ts
@@ -9,11 +9,9 @@ await pool.execute('SET FOREIGN_KEY_CHECKS = 0');
 
 await pool.execute('TRUNCATE TABLE `pages`');
 
-await Promise.all(
-	tables.filter(({ table_name }) => table_name !== 'pages').map(table =>
-		pool.execute(`TRUNCATE TABLE ${table.table_name}`)
-	)
-);
+for (const { table_name } of tables.filter(({ table_name }) => table_name !== 'pages')) {
+	await pool.execute(`TRUNCATE TABLE ${table_name}`);
+}
 
 await pool.execute('SET FOREIGN_KEY_CHECKS = 1');
 await pool.end();
