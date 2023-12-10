228018b1e952596dc47d3261ea373d835580db9e
asasinmode <skin9470@gmail.com>
2023-12-10 08:39:41 +0100

    fix: update global css write path

--- api/src/routes/globalCss.ts
+++ api/src/routes/globalCss.ts
@@ -1,14 +1,14 @@
-import { fileURLToPath } from 'node:url';
 import { writeFile } from 'node:fs/promises';
 import { Hono } from 'hono';
 import { object, string } from 'valibot';
-import { wrap } from 'src/helpers';
+import { wrap } from '../helpers';
+import { adminStylesheetsPath } from '../helpers/files';
 
 export const app = new Hono()
 	.post('/', wrap('json', object({ value: string() })), async (c) => {
 		const { value } = c.req.valid('json');
 
-		await writeFile(fileURLToPath(new URL('../../public/stylesheets/global.css', import.meta.url)), value);
+		await writeFile(`${adminStylesheetsPath}/global.css`, value);
 
 		return c.text('', 201);
 	});
