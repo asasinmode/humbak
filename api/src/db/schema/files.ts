9775cfad1ed9f9c9ba6b9db6e815b0dbea13765f
asasinmode <skin9470@gmail.com>
2023-12-11 20:07:52 +0100

    feat: init files table

--- api/src/db/schema/files.ts
+++ api/src/db/schema/files.ts
@@ -1,12 +1,16 @@
 import { sql } from 'drizzle-orm';
 import { type AnyMySqlColumn, datetime, int, mysqlTable, text } from 'drizzle-orm/mysql-core';
 import { integer, number, object, optional, string } from 'valibot';
+import { directories } from './directories';
 
-export const directories = mysqlTable('directories', {
+export const files = mysqlTable('files', {
 	id: int('id').primaryKey(),
-	parentId: int('parentId').references((): AnyMySqlColumn => directories.id),
+	parentId: int('directoryId').references((): AnyMySqlColumn => directories.id),
 	path: text('path').notNull().default(''),
 	name: text('name').notNull(),
+	title: text('title').notNull(),
+	alt: text('alt').notNull(),
+	mimetype: text('mimetype').notNull(),
 	updatedAt: datetime('updatedAt').notNull().default(sql`NOW()`),
 	createdAt: datetime('createdAt').notNull().default(sql`NOW()`),
 });
