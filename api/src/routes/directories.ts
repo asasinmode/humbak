27b86842bd6f68384149e80cd62ddcca892f4c88
asasinmode <skin9470@gmail.com>
2023-12-13 21:50:41 +0100

    feat: single dir route

--- api/src/routes/directories.ts
+++ api/src/routes/directories.ts
@@ -1,5 +1,8 @@
 import { Hono } from 'hono';
-import { directories } from 'src/db/schema/directories';
+import { eq, isNull } from 'drizzle-orm';
+import { custom, object, string, transform } from 'valibot';
+import { directories } from '../db/schema/directories';
+import { wrap } from '../helpers';
 import { db } from '../db';
 
 export const app = new Hono()
@@ -13,6 +16,38 @@ export const app = new Hono()
 			.from(directories);
 
 		return c.json(result);
-	});
+	})
+	.get(
+		'/:id',
+		wrap('param', transform(object({
+			id: string([
+				custom((v) => {
+					if (v === 'null') {
+						return true;
+					}
+					if (Number.isNaN(Number.parseInt(v))) {
+						return false;
+					}
+					return true;
+				}, 'musi być liczbą lub null'),
+			]),
+		}), ({ id }) => ({
+			id: id === 'null' ? null : Number.parseInt(id),
+		}))),
+		async (c) => {
+			const { id } = c.req.valid('param');
+
+			const result = await db
+				.select({
+					id: directories.id,
+					parentId: directories.parentId,
+					name: directories.name,
+				})
+				.from(directories)
+				.where(id === null ? isNull(directories.parentId) : eq(directories.parentId, id));
+
+			return c.json(result);
+		}
+	);
 
 export type AppType = typeof app;
