09b0a2041d9d035aa34a0f30a43949451e3861f4
asasinmode <skin9470@gmail.com>
2024-01-03 19:44:29 +0100

    feat: add build wrong path todo

--- api/src/helpers/files.ts
+++ api/src/helpers/files.ts
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'node:url';
 import { join } from 'node:path';
 import { env } from '../env';
 
+// todo building makes this path invalid, gotta go only 1 up
 const dirname = fileURLToPath(new URL('../..', import.meta.url));
 
 const adminPublicPath = join(dirname, env.ADMIN_PUBLIC_PATH);
