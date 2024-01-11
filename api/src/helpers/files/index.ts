6fc40f6c3a667a6fd2ab9358802a9fe49c0de6ba
asasinmode <skin9470@gmail.com>
2024-01-11 18:31:40 +0100

    fix: files path after moving

--- api/src/helpers/files/index.ts
+++ api/src/helpers/files/index.ts
@@ -3,7 +3,7 @@ import { join } from 'node:path';
 import { env } from '../../env';
 
 // todo building makes this path invalid, gotta go only 1 up
-const dirname = fileURLToPath(new URL('../..', import.meta.url));
+const dirname = fileURLToPath(new URL('../../..', import.meta.url));
 
 const adminPublicPath = join(dirname, env.ADMIN_PUBLIC_PATH);
 
