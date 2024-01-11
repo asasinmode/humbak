603a9d1c58250c5c2b645783351d9fb99ca633da
asasinmode <skin9470@gmail.com>
2024-01-11 21:39:15 +0100

    test: files moved to same loc

--- api/__tests__/fileEditValidation.test.ts
+++ api/__tests__/fileEditValidation.test.ts
@@ -19,22 +19,71 @@ test('file edit validation', { only: true }, async (t) => {
 		});
 	});
 
-	await t.test('errors multiple of the same', async () => {
-		const originalFiles = createOriginalFiles([]);
+	await t.test('errors 2 moved to same location', async () => {
+		const originalFiles = createOriginalFiles([
+			{ directoryId: null },
+			{ directoryId: null },
+		]);
 
 		const result = await getFilesToEdit(new Map(), new Map(), [], originalFiles, [
-			createInputFile(1, null),
-			createInputFile(1, null),
+			createInputFile(1, null, '1'),
+			createInputFile(2, null, '1'),
 		]);
 
 		assert.deepStrictEqual(result.filesToEdit, []);
 		assert.deepStrictEqual(result.errors, {
 			0: {
-				id: 'tylko jedna instrukcja może być wysłana naraz',
+				name: 'wiele plików nie może być przeniesione w to samo miejsce',
 			},
 			1: {
-				id: 'tylko jedna instrukcja może być wysłana naraz',
+				name: 'wiele plików nie może być przeniesione w to samo miejsce',
 			},
 		});
 	});
+
+	await t.test('errors 3 moved to same location', async () => {
+		const originalFiles = createOriginalFiles([
+			{ directoryId: null },
+			{ directoryId: null },
+			{ directoryId: null },
+		]);
+
+		const result = await getFilesToEdit(new Map(), new Map(), [], originalFiles, [
+			createInputFile(1, null, '1'),
+			createInputFile(2, null, '1'),
+			createInputFile(3, null, '1'),
+		]);
+
+		assert.deepStrictEqual(result.filesToEdit, []);
+		assert.deepStrictEqual(result.errors, {
+			0: {
+				name: 'wiele plików nie może być przeniesione w to samo miejsce',
+			},
+			1: {
+				name: 'wiele plików nie może być przeniesione w to samo miejsce',
+			},
+			2: {
+				name: 'wiele plików nie może być przeniesione w to samo miejsce',
+			},
+		});
+	});
+
+	// await t.test('errors multiple of the same', async () => {
+	// 	const originalFiles = createOriginalFiles([]);
+
+	// 	const result = await getFilesToEdit(new Map(), new Map(), [], originalFiles, [
+	// 		createInputFile(1, null),
+	// 		createInputFile(1, null),
+	// 	]);
+
+	// 	assert.deepStrictEqual(result.filesToEdit, []);
+	// 	assert.deepStrictEqual(result.errors, {
+	// 		0: {
+	// 			id: 'tylko jedna instrukcja może być wysłana naraz',
+	// 		},
+	// 		1: {
+	// 			id: 'tylko jedna instrukcja może być wysłana naraz',
+	// 		},
+	// 	});
+	// });
 });
