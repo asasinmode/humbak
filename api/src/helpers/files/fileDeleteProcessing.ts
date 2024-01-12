94e5a80baa8bbe82217d189f16bff199b6819d12
asasinmode <skin9470@gmail.com>
2024-01-12 17:52:36 +0100

    feat: file delete processing

--- api/src/helpers/files/fileDeleteProcessing.ts
+++ api/src/helpers/files/fileDeleteProcessing.ts
@@ -1,6 +1,30 @@
+import { existsSync } from 'node:fs';
+import { rm } from 'node:fs/promises';
+import { inArray } from 'drizzle-orm';
+import { db } from 'src/db';
+import { files } from 'src/db/schema/files';
+import { filesStoragePath } from 'src/helpers/files';
+
 export async function processDeletedFiles(
 	deletedFileIds: number[],
-	affectedPagesIds: Set<number>
+	modifiedFilesIds: Set<number>
 ) {
+	if (!deletedFileIds.length) {
+		return;
+	}
 
+	const originalFiles = await db
+		.select({
+			id: files.id,
+			path: files.path,
+		})
+		.from(files)
+		.where(inArray(files.id, deletedFileIds));
+
+	for (const file of originalFiles) {
+		modifiedFilesIds.add(file.id);
+		existsSync(`${filesStoragePath}${file.path}`) && await rm(`${filesStoragePath}${file.path}`);
+	}
+
+	await db.delete(files).where(inArray(files.id, deletedFileIds));
 }
