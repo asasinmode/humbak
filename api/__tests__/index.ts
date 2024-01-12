1829f21de1fb9d31dbea5c9cb6980417e8346042
asasinmode <skin9470@gmail.com>
2024-01-12 17:53:53 +0100

    test: global setup

--- api/__tests__/index.ts
+++ api/__tests__/index.ts
@@ -1,24 +1,39 @@
 import process from 'node:process';
-import { URL, fileURLToPath } from 'node:url';
-import { finished } from 'node:stream/promises';
+import { fileURLToPath } from 'node:url';
 import { spec } from 'node:test/reporters';
 import { run } from 'node:test';
 import { env } from 'src/env';
-import { filesStoragePath, stylesheetsStoragePath } from 'src/helpers/files';
+import { db, pool } from 'src/db';
+import { migrate } from 'drizzle-orm/mysql2/migrator';
+import { getTableNames } from 'src/helpers';
 
 if (env.NODE_ENV !== 'test') {
 	throw new Error('tests cannot be run in non-test environment');
 }
 
+console.log('running test setup...');
+const setupStart = performance.now();
+
+// todo use pathname instead of fileurltopath
 const dirname = fileURLToPath(new URL('.', import.meta.url));
 
-console.log({ filesStoragePath, stylesheetsStoragePath, dirname });
+await migrate(db, { migrationsFolder: `${dirname}/../src/db/migrations` });
+const tables = await getTableNames();
+await pool.execute('SET FOREIGN_KEY_CHECKS = 0');
+await pool.execute('TRUNCATE TABLE `pages`');
+for (const thing of tables.filter(t => t.table_name !== '__drizzle_migrations')) {
+	await pool.execute(`TRUNCATE TABLE ${thing.table_name}`);
+}
+await pool.execute('SET FOREIGN_KEY_CHECKS = 1');
+
+const setupEnd = performance.now();
+console.log('finished running setup in', setupEnd - setupStart, '\n');
 
 const stream = run({
 	files: [
-		// `${dirname}/dirDeleteValidation.test.ts`,
-		// `${dirname}/dirEditValidation.test.ts`,
-		// `${dirname}/fileEditValidation.test.ts`,
+		`${dirname}/dirDeleteValidation.test.ts`,
+		`${dirname}/dirEditValidation.test.ts`,
+		`${dirname}/fileEditValidation.test.ts`,
 		`${dirname}/fileDeleteProcessing.test.ts`,
 	],
 	only: process.argv.includes('--test-only'),
@@ -26,7 +41,3 @@ const stream = run({
 }).compose(new spec()); /* eslint-disable-line new-cap */
 
 stream.pipe(process.stdout);
-
-await finished(stream);
-
-console.log('finished running tests');
