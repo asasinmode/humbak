a3666412663ab87ca98bdfd2c2ae9005920cbfb9
asasinmode <skin9470@gmail.com>
2024-01-13 10:25:41 +0100

    feat: dir delete processing update updated pages ids

--- api/src/helpers/files/dirDeleteProcessing.ts
+++ api/src/helpers/files/dirDeleteProcessing.ts
@@ -1,15 +1,17 @@
 import { existsSync } from 'node:fs';
 import { rm } from 'node:fs/promises';
-import { inArray } from 'drizzle-orm';
+import { eq, inArray } from 'drizzle-orm';
 import { db } from 'src/db';
 import { filesStoragePath } from 'src/helpers/files';
 import type { IDir } from 'src/routes/directories';
 import { directories } from 'src/db/schema/directories';
+import { filesToPages } from 'src/db/schema/filesToPages';
+import { files } from 'src/db/schema/files';
 
 export async function processDeletedDirs(
 	input: Map<number, IDir>,
 	allDirs: Map<number, IDir>,
-	modifiedDirsIds: Set<number>
+	modifiedPagesIds: Set<number>
 ) {
 	if (!input.size) {
 		return;
@@ -18,11 +20,22 @@ export async function processDeletedDirs(
 	const dirIdsToDelete: number[] = [];
 	for (const [id, dir] of input.entries()) {
 		dirIdsToDelete.push(id);
-		modifiedDirsIds.add(id);
 
 		const path = `${filesStoragePath}${dir.path}`;
 		existsSync(path) && await rm(path, { recursive: true });
 		allDirs.delete(dir.id);
+	}
+
+	const affectedPageIds = await db
+		.selectDistinct({
+			pageId: filesToPages.pageId,
+		})
+		.from(filesToPages)
+		.leftJoin(files, eq(files.id, filesToPages.fileId))
+		.where(inArray(files.directoryId, dirIdsToDelete));
+
+	for (const { pageId } of affectedPageIds) {
+		modifiedPagesIds.add(pageId);
 	}
 
 	await db.delete(directories).where(inArray(directories.id, dirIdsToDelete));
