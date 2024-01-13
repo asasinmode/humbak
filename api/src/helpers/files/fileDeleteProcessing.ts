3de962ad1eebcce3d40b4c50475015cd03fa2ec6
asasinmode <skin9470@gmail.com>
2024-01-13 09:48:09 +0100

    feat: file delete processing update modified page ids

--- api/src/helpers/files/fileDeleteProcessing.ts
+++ api/src/helpers/files/fileDeleteProcessing.ts
@@ -4,8 +4,9 @@ import { inArray } from 'drizzle-orm';
 import { db } from 'src/db';
 import { files } from 'src/db/schema/files';
 import { filesStoragePath } from 'src/helpers/files';
+import { filesToPages } from 'src/db/schema/filesToPages';
 
-export async function processDeletedFiles(input: number[], modifiedFilesIds: Set<number>) {
+export async function processDeletedFiles(input: number[], modifiedPagesIds: Set<number>) {
 	if (!input.length) {
 		return;
 	}
@@ -19,8 +20,18 @@ export async function processDeletedFiles(input: number[], modifiedFilesIds: Set
 		.where(inArray(files.id, input));
 
 	for (const file of originalFiles) {
-		modifiedFilesIds.add(file.id);
 		existsSync(`${filesStoragePath}${file.path}`) && await rm(`${filesStoragePath}${file.path}`);
+	}
+
+	const affectedPageIds = await db
+		.selectDistinct({
+			pageId: filesToPages.pageId,
+		})
+		.from(filesToPages)
+		.where(inArray(filesToPages.fileId, input));
+
+	for (const { pageId } of affectedPageIds) {
+		modifiedPagesIds.add(pageId);
 	}
 
 	await db.delete(files).where(inArray(files.id, input));
