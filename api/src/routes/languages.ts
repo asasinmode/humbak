912e3ddc0ac6368da76ca98e764e3429876e054a
asasinmode <skin9470@gmail.com>
2024-01-13 16:04:36 +0100

    feat: languages order desc

--- api/src/routes/languages.ts
+++ api/src/routes/languages.ts
@@ -1,10 +1,11 @@
 import { Hono } from 'hono';
+import { desc } from 'drizzle-orm';
 import { db } from '../db';
 import { pages } from '../db/schema/pages';
 
 export const app = new Hono()
 	.get('/', async (c) => {
-		const result = await db.selectDistinct({ language: pages.language }).from(pages).orderBy(pages.createdAt);
+		const result = await db.selectDistinct({ language: pages.language }).from(pages).orderBy(desc(pages.language));
 
 		return c.json(result.map(item => item.language));
 	});
