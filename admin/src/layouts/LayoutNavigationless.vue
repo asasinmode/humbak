8528bf217efcf64eaa9d5e0172648ff7fc5ea41a
asasinmode <skin9470@gmail.com>
2024-01-17 19:56:29 +0100

    feat: layout navigationless add theme toggle

--- admin/src/layouts/LayoutNavigationless.vue
+++ admin/src/layouts/LayoutNavigationless.vue
@@ -1,3 +1,6 @@
 <template>
+	<TheThemeToggle
+		class="fixed filter-drop-shadow top-2 filter-drop-shadow-color-black/20 left-2 md:(left-[clamp(0.25rem,-4.06rem_+_6.73vw,2rem)] mt-0)"
+	/>
 	<RouterView />
 </template>
