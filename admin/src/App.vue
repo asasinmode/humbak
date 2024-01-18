99a50a130ad541a181f20bb0101beac35d4b374b
asasinmode <skin9470@gmail.com>
2024-01-18 19:53:04 +0100

    refactor: move auth init to app

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -2,14 +2,37 @@
 import LayoutDefault from '~/layouts/LayoutDefault.vue';
 
 const router = useRouter();
+const api = useApi();
 const { TheConfirm } = useConfirm();
-const { init } = useAuth();
+const { logout, login } = useAuth(router);
 
-onMounted(async () => init(router));
+const isVerifying = ref(false);
+
+onMounted(async () => {
+	const storedUsername = localStorage.getItem('username');
+	const storedJwt = localStorage.getItem('jwt');
+	if (!storedUsername || !storedJwt) {
+		return;
+	}
+
+	isVerifying.value = true;
+	try {
+		await new Promise(resolve => setTimeout(resolve, 2000));
+		// @ts-expect-error idk why types are wrong
+		await api.auth.verify.$get();
+		login(storedUsername, storedJwt);
+	} catch (error) {
+		console.error(error);
+		logout();
+	} finally {
+		isVerifying.value = false;
+	}
+});
 </script>
 
 <template>
-	<component :is="$route.meta.layout ?? LayoutDefault">
+	<VLoading v-if="isVerifying" class="absolute inset-0 flex-center" :size="30" />
+	<component :is="$route.meta.layout ?? LayoutDefault" v-else>
 		<slot />
 	</component>
 	<TheConfirm />
--- admin/src/composables/useAuth.ts
+++ admin/src/composables/useAuth.ts
@@ -1,13 +1,8 @@
-import type { Router } from 'vue-router';
-
-const api = useApi();
-
 const username = ref('');
 const jwt = ref('');
 const isLoggedIn = ref(false);
-const isVerifying = ref(false);
 
-export function useAuth(router?: Router) {
+export function useAuth() {
 	function login(user: string, token: string) {
 		username.value = user;
 		jwt.value = token;
@@ -15,8 +10,6 @@ export function useAuth(router?: Router) {
 
 		localStorage.setItem('username', user);
 		localStorage.setItem('jwt', token);
-
-		router?.push('/');
 	}
 
 	function logout() {
@@ -26,41 +19,13 @@ export function useAuth(router?: Router) {
 
 		localStorage.setItem('username', '');
 		localStorage.setItem('jwt', '');
-
-		router?.push('/login');
-	}
-
-	async function init() {
-		const storedUsername = localStorage.getItem('username');
-		const storedJwt = localStorage.getItem('jwt');
-
-		if (!storedUsername || !storedJwt) {
-			return;
-		}
-
-		isVerifying.value = true;
-
-		try {
-			// @ts-expect-error idk why types are wrong
-			await api.auth.verify.$get();
-			username.value = storedUsername;
-			jwt.value = storedJwt;
-			isLoggedIn.value = true;
-		} catch (error) {
-			console.error(error);
-			logout();
-		} finally {
-			isVerifying.value = false;
-		}
 	}
 
 	return {
 		username,
 		jwt,
 		isLoggedIn,
-		isVerifying,
 		login,
 		logout,
-		init,
 	};
 }
