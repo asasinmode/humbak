42e9b2dbc6f52377c77780e9a7fc68a0c1a9e54a
asasinmode <skin9470@gmail.com>
2024-01-18 20:06:51 +0100

    feat: align navigationless theme toggle

--- admin/src/layouts/LayoutNavigationless.vue
+++ admin/src/layouts/LayoutNavigationless.vue
@@ -1,6 +1,6 @@
 <template>
 	<TheThemeToggle
-		class="fixed filter-drop-shadow top-2 filter-drop-shadow-color-black/20 left-2 md:(left-[clamp(0.25rem,-4.06rem_+_6.73vw,2rem)] mt-0)"
+		class="fixed filter-drop-shadow top-[0.8125rem] filter-drop-shadow-color-black/20 left-2 md:left-[clamp(0.25rem,-4.06rem_+_6.73vw,2rem)]"
 	/>
 	<RouterView />
 </template>
