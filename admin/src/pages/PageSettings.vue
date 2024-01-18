3095f5756473afab7fc4f4d3d123ed6e45f825f7
asasinmode <skin9470@gmail.com>
2024-01-18 21:47:13 +0100

    feat: settings logout button layout

--- admin/src/pages/PageSettings.vue
+++ admin/src/pages/PageSettings.vue
@@ -9,10 +9,12 @@ function logout() {
 </script>
 
 <template>
-	<main id="content" class="flex-center flex-col">
-		ustawienia
-		<VButton class="neon-yellow" @click="logout">
-			wyloguj się
-		</VButton>
+	<main id="content" class="flex flex-col gap-x-3 gap-y-5 pb-4 pt-[1.125rem]">
+		<div class="mx-auto max-w-360 w-full md:justify-end flex gap-x-3 px-container">
+			<VButton class="neon-blue w-fit" @click="logout">
+				<div class="i-ph-sign-out inline-block align-sub text-blue" />
+				wyloguj się
+			</VButton>
+		</div>
 	</main>
 </template>
