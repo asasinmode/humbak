a5c6bca29b72c1c692a31884e659b3840173280f
asasinmode <skin9470@gmail.com>
2024-01-22 19:50:53 +0100

    feat: init page

---
+++ page/error.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+	error?: {
+		statusCode: string;
+		message: string;
+	};
+}>();
+
+useHead({
+	title: 'error',
+});
+</script>
+
+<template>
+	<main class="col-span-full row-span-full flex-center flex-col gap-4">
+		<h1 class="text-6xl font-bold">
+			{{ error?.statusCode }}
+		</h1>
+		<h2 class="text-center text-3xl">
+			{{ error?.message }}
+		</h2>
+		<NuxtLink to="/" class="text-xl text-link" @click="clearError">
+			go back
+		</NuxtLink>
+	</main>
+</template>
---
+++ page/nuxt.config.ts
@@ -0,0 +1,21 @@
+export default defineNuxtConfig({
+	devtools: { enabled: true },
+	modules: [
+		'@unocss/nuxt',
+	],
+	css: [
+		'@unocss/reset/tailwind.css',
+		'assets/index.css',
+	],
+	app: {
+		head: {
+			link: [{ rel: 'icon', href: '/favicon.ico' }],
+			script: [{
+				children: `const prefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+const setting = localStorage.getItem('color-scheme') || 'auto'
+if (setting === 'dark' || (prefersDark && setting !== 'light'))
+document.documentElement.classList.toggle('dark', true)`,
+			}],
+		},
+	},
+});
