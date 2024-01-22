e6d93bdbe26d37f8eb4fd97b04a24eaaec1c7509
asasinmode <skin9470@gmail.com>
2024-01-22 20:09:35 +0100

    fix: cleanup no pinia in admin

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
 import { router } from './router';
 import App from './App.vue';
 
@@ -7,9 +6,7 @@ import '@unocss/reset/tailwind.css';
 import 'virtual:uno.css';
 import './assets/index.css';
 
-const pinia = createPinia();
 const app = createApp(App);
 
 app.use(router);
-app.use(pinia);
 app.mount('body');
--- admin/src/store/default.ts
+++
@@ -1,5 +0,0 @@
-import { defineStore } from 'pinia';
-
-export const useDefaultStore = defineStore('index', () => {
-	return { };
-});
