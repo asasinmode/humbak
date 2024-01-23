f1217f29b36e56335f26fd5586e1e6217d452bce
asasinmode <skin9470@gmail.com>
2024-01-23 18:19:36 +0100

    feat: page routes

--- page/pages/[[language]].vue
+++ page/pages/[[language]].vue
@@ -1,16 +1,30 @@
 <script setup lang="ts">
+definePageMeta({
+	middleware: (route) => {
+		if (!route.params.language) {
+			return navigateTo('/pl');
+		}
+	},
+});
+
 const { public: { apiUrl } } = useRuntimeConfig();
 
 const { data: languages, error: languagesError } = await useFetch<string[]>(`${apiUrl}/public/languages`);
 
-if (!languages.value || languagesError.value) {
+if (languagesError.value || !languages.value?.length) {
 	console.error(languagesError.value);
 	throw createError({ message: 'server error', statusCode: 500 });
 }
+
+const route = useRoute();
+const { language } = route.params;
+
+console.log('language setup', route, language);
 </script>
 
 <template>
 	<main>
 		hello {{ languages }}
+		<NuxtPage />
 	</main>
 </template>
---
+++ page/pages/[[language]]/[[slug]].vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+console.log('slug setup');
+</script>
+
+<template>
+	<div>
+		hello from slug
+	</div>
+</template>
