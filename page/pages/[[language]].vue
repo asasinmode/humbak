5e191d44e7fcd72764a02326709801257313c708
asasinmode <skin9470@gmail.com>
2024-01-23 19:03:06 +0100

    feat: fetch page data language

--- page/pages/[[language]].vue
+++ page/pages/[[language]].vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import type { ILanguagePageData } from '~/types/api';
+
 definePageMeta({
 	middleware: (route) => {
 		if (!route.params.language) {
@@ -10,7 +12,6 @@ definePageMeta({
 const { public: { apiUrl } } = useRuntimeConfig();
 
 const { data: languages, error: languagesError } = await useFetch<string[]>(`${apiUrl}/public/languages`);
-
 if (languagesError.value || !languages.value?.length) {
 	console.error(languagesError.value);
 	throw createError({ message: 'server error', statusCode: 500 });
@@ -19,12 +20,26 @@ if (languagesError.value || !languages.value?.length) {
 const route = useRoute();
 const { language } = route.params;
 
-console.log('language setup', route, language);
+if (typeof language !== 'string') {
+	throw createError({ message: 'server error', statusCode: 500 });
+}
+
+if (!languages.value.includes(language)) {
+	throw createError({ message: 'nieznany język', statusCode: 404 });
+}
+
+const { data: languageData, error: languageDataError } = await useFetch<ILanguagePageData>(`${apiUrl}/public/${language}`);
+if (languagesError.value || !languageData.value) {
+	console.error(languageDataError.value);
+	throw createError({ statusCode: 500, message: 'błąd przy ładowaniu danych strony' });
+}
+
+console.log('language data', languageData.value);
 </script>
 
 <template>
 	<main>
-		hello {{ languages }}
+		hello {{ languageData }}
 		<NuxtPage />
 	</main>
 </template>
--- page/types/api.ts
+++ page/types/api.ts
@@ -3,7 +3,7 @@ import type { AppType } from '@humbak/api/src';
 
 type Client = ReturnType<typeof hc<AppType>>;
 
-export type ILanguageResponse = InferResponseType<Client['public'][':language']['$get']>;
+export type ILanguagePageData = InferResponseType<Client['public'][':language']['$get']>;
 
 export type ISlide = ILanguageResponse['slides'][number];
 export type IMenuLink = ILanguageResponse['menuLinks'][number];
