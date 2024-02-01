755b4685bd4f0c542c172a380db5adc5f55bada9
asasinmode <skin9470@gmail.com>
2024-02-01 21:33:33 +0100

    feat: add comment with todos left

--- api/src/helpers/files/image.ts
+++ api/src/helpers/files/image.ts
@@ -4,6 +4,19 @@ import sharp from 'sharp';
 
 sharp.cache(false);
 
+// dont create sizes for svg & update preview on frontend
+// validation if file image check name without extension for images & tests
+// change bad meta value toast to "incorrect"
+// add veditor errors (meta incorrect json)
+// humbak image on frontend replace use absolute path to page
+// add default language on page, language select use it or first
+// footer location inputs add placeholders Where to find us / https://google.com/maps
+// files not image span extension if no mimetype
+// store file view preference in localstorage
+// page language change menulink move to hidden & reset position
+// create page default menu link hidden
+// images title & alt per language (separate table)
+// page use global meta
 export async function createImageSizes(
 	path: string,
 	mimetype: string
