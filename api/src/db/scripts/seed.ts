b5ce862f157e85d0f3b46a993b1969df16380c3e
asasinmode <skin9470@gmail.com>
2024-02-09 17:23:19 +0100

    feat: seed slides

--- api/src/db/scripts/seed.ts
+++ api/src/db/scripts/seed.ts
@@ -17,20 +17,19 @@ import { users } from '../schema/users';
 
 await promptProdContinue();
 
-await writeFile(
-	`${filesStoragePath}/slideImage.jpg`,
-	await fetch('https://images.unsplash.com/photo-1455577380025-4321f1e1dca7?q=80&w=1920').then(r => r.arrayBuffer()).then(v => Buffer.from(v))
-);
-const { width, height } = await createImageSizes(`${filesStoragePath}/river.jpg`, 'image/jpeg');
-const [{ insertId: riverSlideImageId }] = await db.insert(files).values({
+await writeFile(`${stylesheetsStoragePath}/global.css`, `.text-slider {
+  font-size: clamp(3rem, -0.7895rem + 18.9474vi, 12rem);
+  font-weight: 600;
+}`);
+
+const riverSlideImageId = await createFile({
+	url: 'https://images.unsplash.com/photo-1455577380025-4321f1e1dca7',
 	directoryId: null,
 	name: 'river.jpg',
 	path: '/river.jpg',
-	title: 'river in a forest',
+	title: 'river',
 	alt: 'a river surrounded by a forest',
 	mimetype: 'image/jpeg',
-	width,
-	height,
 });
 
 await mkdir(`${filesStoragePath}/oceans`);
@@ -47,6 +46,34 @@ await mkdir(`${filesStoragePath}/lakes`);
 const [{ insertId: lakesDirId }] = await db.insert(directories).values({
 	name: 'lakes',
 	path: '/lakes',
+});
+
+const oceanSlideImageId = await createFile({
+	url: 'https://images.unsplash.com/photo-1488278905738-514111aa236c',
+	directoryId: oceansDirId,
+	name: 'ocean.jpg',
+	path: '/oceans/ocean.jpg',
+	title: 'ocean',
+	alt: 'an ocean under a clouded sky',
+	mimetype: 'image/jpeg',
+});
+const seaSlideImageId = await createFile({
+	url: 'https://images.unsplash.com/photo-1444260239795-df1f0772a252',
+	directoryId: seasDirId,
+	name: 'sea.jpg',
+	path: '/seas/sea.jpg',
+	title: 'sea',
+	alt: 'aerial photo of a see with a rocky coast',
+	mimetype: 'image/jpeg',
+});
+const lakeSlideImageId = await createFile({
+	url: 'https://images.unsplash.com/photo-1516132006923-6cf348e5dee2',
+	directoryId: lakesDirId,
+	name: 'lake.jpg',
+	path: '/lakes/lake.jpg',
+	title: 'lake',
+	alt: 'a lake surrounded by a forest and mountains',
+	mimetype: 'image/jpeg',
 });
 
 await mkdir(`${filesStoragePath}/other`);
@@ -90,39 +117,72 @@ const slidesData = [
 	{
 		name: 'ocean',
 		language: 'en',
-		content: `<div class="flex-center w-full h-full" style="background-color: hsla(0, 100%, 50%, 0.3)">
-<h6>1</h6>
+		content: `<div class="flex-center w-full h-full relative">
+  <HumbakFile fid="${oceanSlideImageId}" class="w-full h-full"></HumbakFile>
+  <h6 class="absolute w-full h-full flex-center text-slider" style="color: white; background-color: hsl(0 0 0% / 0.2)">
+    ocean
+  </h6>
+  <a
+    href="https://unsplash.com/photos/landscape-photography-of-waves-and-clouds-q-DJ9XhKkhA"
+    target="_blank"
+    class="absolute text-link"
+    style="bottom: 0.3rem; right: 0.5rem;"
+  >
+    unsplash
+  </a>
 </div>`,
 	},
 	{
 		name: 'sea',
 		language: 'en',
-		content: `<div class="flex-center w-full h-full" style="background-color: hsla(0, 100%, 50%, 0.3)">
-<h6>1</h6>
+		content: `<div class="flex-center w-full h-full relative">
+  <HumbakFile fid="${seaSlideImageId}" class="w-full h-full"></HumbakFile>
+  <h6 class="absolute w-full h-full flex-center text-slider" style="color: white; background-color: hsl(0 0 0% / 0.2)">
+    sea
+  </h6>
+  <a
+    href="https://unsplash.com/photos/landscape-photography-of-waves-and-clouds-q-DJ9XhKkhA"
+    target="_blank"
+    class="absolute text-link"
+    style="bottom: 0.3rem; right: 0.5rem;"
+  >
+    unsplash
+  </a>
 </div>`,
 	},
 	{
 		name: 'lake',
 		language: 'en',
-		content: `<div class="flex-center w-full h-full" style="background-color: hsla(120, 100%, 50%, 0.3)">
-<h6>2</h6>
+		content: `<div class="flex-center w-full h-full relative">
+  <HumbakFile fid="${lakeSlideImageId}" class="w-full h-full"></HumbakFile>
+  <h6 class="absolute w-full h-full flex-center text-slider" style="color: white; background-color: hsl(0 0 0% / 0.2)">
+    lake
+  </h6>
+  <a
+    href="https://unsplash.com/photos/landscape-photography-of-waves-and-clouds-q-DJ9XhKkhA"
+    target="_blank"
+    class="absolute text-link"
+    style="bottom: 0.3rem; right: 0.5rem;"
+  >
+    unsplash
+  </a>
 </div>`,
 	},
 	{
 		name: 'river',
 		language: 'en',
-		content: `<div class="flex-center w-full h-full">
+		content: `<div class="flex-center w-full h-full relative">
   <HumbakFile fid="${riverSlideImageId}" class="w-full h-full"></HumbakFile>
-  <h6 class="absolute" style="font-size: 8rem; color: white; font-weight: 600;">
-    River
+  <h6 class="absolute text-slider w-full h-full flex-center" style="color: white; background-color: hsl(0 0% 0% / 0.2)">
+    river
   </h6>
-  <h6 class="absolute" style="font-size: 8rem; color: #00ff00; font-weight: 600; transform: translate(2px, 2px); mix-blend-mode: difference;">
-    River
+  <h6 class="absolute text-slider" style="color: #00ff00; transform: translate(2px, 2px); mix-blend-mode: difference;">
+    river
   </h6>
-  <h6 class="absolute" style="font-size: 8rem; color: #ff00ff; font-weight: 600; transform: translate(-2px, -2px); mix-blend-mode: difference;">
-    River
+  <h6 class="absolute text-slider" style="color: #ff00ff; transform: translate(-2px, -2px); mix-blend-mode: difference;">
+    river
   </h6>
-	<a
+  <a
     href="https://unsplash.com/photos/body-of-water-between-trees-under-cloudy-sky-rB7-LCa_diU"
     target="_blank"
     class="absolute text-link"
@@ -192,3 +252,26 @@ await db.insert(users).values({
 });
 
 await pool.end();
+
+async function createFile(
+	{ url, name, directoryId, path, title, alt, mimetype }:
+	{ url: string; name: string; directoryId: number | null; path: string; title: string; alt: string; mimetype: string; }
+) {
+	await writeFile(
+	`${filesStoragePath}${path}`,
+	await fetch(`${url}?q=80&w=1920`).then(r => r.arrayBuffer()).then(v => Buffer.from(v))
+	);
+	const { width, height } = await createImageSizes(`${filesStoragePath}${path}`, mimetype);
+	const [{ insertId }] = await db.insert(files).values({
+		directoryId,
+		name,
+		path,
+		title,
+		alt,
+		mimetype,
+		width,
+		height,
+	});
+
+	return insertId;
+}
