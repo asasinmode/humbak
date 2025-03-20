481adec2599b5603495c2df839f5402a5f919461
asasinmode <skin9470@gmail.com>
2025-03-20 22:57:05 +0100

    fix: consistent humbak file error color

--- api/src/routes/languages.ts
+++ api/src/routes/languages.ts
@@ -5,7 +5,10 @@ import { pages } from '../db/schema/pages';
 
 export const app = new Hono()
 	.get('/', async (c) => {
-		const result = await db.selectDistinct({ language: pages.language }).from(pages).orderBy(desc(pages.language));
+		const result = await db
+			.selectDistinct({ language: pages.language })
+			.from(pages)
+			.orderBy(desc(pages.language));
 
 		return c.json(result.map(item => item.language));
 	});
